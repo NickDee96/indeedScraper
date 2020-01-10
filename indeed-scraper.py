dcc4798675b40b8d624d2a2b25cd7ec08f2346be
nickdee96 <mumeronick@gmail.com>
2020-01-10 15:43:50 +0300

    feat: minor changes

--- indeed-scraper.py
+++ indeed-scraper.py
@@ -7,20 +7,23 @@ from multiprocessing import Pool
 
 data=pd.read_csv("data/jobs2.csv")
 def scraper(start):
-    stop=start+200
+    stop=start+400
     with open("data/jd.csv","a",newline="") as csvfile:
         writer=csv.DictWriter(csvfile,fieldnames=["Role","jobkey","jd"])
+        writer.writeheader()
         for i in range(start,stop):
-            page=soup(req.get(data["url"][i]).content,"lxml")
-            text=page.find_all("div",{"id":"jobDescriptionText"})[0].text
-            #text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
-            writer.writerow({"Role":data.Role[i],
-                            "jobkey":data.jobkey[i],
-                            "jd":text})
-            print(i)
+            try:
+                page=soup(req.get(data["url"][i]).content,"lxml")
+                text=page.find_all("div",{"id":"jobDescriptionText"})[0].text
+                writer.writerow({"Role":data.Role[i],
+                                "jobkey":data.jobkey[i],
+                                "jd":text})
+                print(i)
+            except KeyError:
+                break
 
 
-
-p = Pool(10)
-p.map(scraper, range(0,len(data),200))
+if __name__ == "__main__":    
+    p = Pool(10)
+    p.map(scraper, range(0,len(data),200))
 
