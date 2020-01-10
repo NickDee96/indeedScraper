9a74f632f7682a664091715533f01d3bcfa65968
nickdee96 <mumeronick@gmail.com>
2020-01-10 12:15:30 +0300

    feat: Scraper update and Multiprocessing function added

--- indeed-api.py
+++ indeed-api.py
@@ -1,42 +1,65 @@
 import requests as req
 import csv
 import sys
-def getjds(role,filename,length):
-    fieldnames=[
-            "jobtitle",
-            "company",
-            "city",
-            "state",
-            "country",
-            "language",
-            "formattedLocation",
-            "source",
-            "date",
-            "snippet",
-            "url",
-            "onmousedown",
-            "latitude",
-            "longitude",
-            "jobkey",
-            "sponsored",
-            "expired",
-            "indeedApply",
-            "formattedLocationFull",
-            "formattedRelativeTime",
-            "stations"]
-    url="http://api.indeed.com/ads/apisearch?publisher=9091824477922251&q={}&sort=&radius=&st=&jt=&start={}&limit=25&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&format=json&useragent=Mozilla/%2F4.0(Firefox)&v=2"
-    with open(filename,"w",newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0,length,25):
-            mUrl=url.format(role,i)
-            data=req.get(mUrl).json()
-            for j in data["results"]:
-                writer.writerow(j)
-            print(i) 
 
-if __name__ == "__main__":
-    role=str(sys.argv[1])
-    filename=str(sys.argv[2])
-    length=int(sys.argv[3])
-    getjds(role,filename,length)     
+countries={
+    "ma":"Morocco",
+    "za":"South Africa",
+    "ng":"Nigeria",
+    "eg":"Egypt"
+}
+with open("roles.txt","r") as rFile:
+    roles=[x.strip() for x in rFile.readlines()]
+
+
+fieldnames=[
+        "Role",
+        "jobtitle",
+        "company",
+        "city",
+        "state",
+        "country",
+        "language",
+        "formattedLocation",
+        "source",
+        "date",
+        "snippet",
+        "url",
+        "onmousedown",
+        "latitude",
+        "longitude",
+        "jobkey",
+        "sponsored",
+        "expired",
+        "indeedApply",
+        "formattedLocationFull",
+        "formattedRelativeTime",
+        "stations"]
+url="http://api.indeed.com/ads/apisearch?publisher=9091824477922251&q={}&sort=&radius=&st=&jt=&start={}&limit=25&fromage=7&filter=1&latlong=1&co={}&chnl=&userip=1.2.3.4&format=json&useragent=Mozilla/%2F4.0(Firefox)&v=2"
+
+with open("data/jobs2.csv","w",newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for role in roles:
+        for code in countries.keys():
+            try:
+                location=req.get(url.format(role,0,code)).json()["results"][0]["country"]
+            except IndexError:
+                print("passed for {} in {}".format(role,countries[code]))
+                pass
+            length=req.get(url.format(role,0,code)).json()["totalResults"]
+            if location=="US":
+                print("passed for {} in {}".format(role,countries[code]))
+                pass
+            else:
+                print("Getting for {} in {}".format(role,countries[code]))
+                for i in range(0,length,25):
+                    mUrl=url.format(role,i,code)
+                    data=req.get(mUrl).json()
+                    for j in data["results"]:
+                        j.update({
+                            "Role":role
+                        })
+                        writer.writerow(j)
+                    print(i) 
+
--- indeed-scraper.py
+++ indeed-scraper.py
@@ -2,15 +2,25 @@ from bs4 import BeautifulSoup as soup
 import requests as req
 import pandas as pd
 import csv
-data=pd.read_csv("web_developer.csv")
-with open("C:\\Users\\nickdee96\\Documents\\Linux upload\\glassdoor upload\\sanity1.csv","a",newline="") as csvfile:
-    writer=csv.DictWriter(csvfile,fieldnames=["level","text"])
-    for i in range(len(data)):
-        page=soup(req.get(data["url"][i]).text,'lxml')
-        text=page.find_all('div',{'id':'jobDescriptionText'})[0].text
-        text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
-        writer.writerow({"level":"web developer",
-                        "text":text})
-        print(i)
 
+from multiprocessing import Pool
+
+data=pd.read_csv("data/jobs2.csv")
+def scraper(start):
+    stop=start+200
+    with open("data/jd.csv","a",newline="") as csvfile:
+        writer=csv.DictWriter(csvfile,fieldnames=["Role","jobkey","jd"])
+        for i in range(start,stop):
+            page=soup(req.get(data["url"][i]).content,"lxml")
+            text=page.find_all("div",{"id":"jobDescriptionText"})[0].text
+            #text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
+            writer.writerow({"Role":data.Role[i],
+                            "jobkey":data.jobkey[i],
+                            "jd":text})
+            print(i)
+
+
+
+p = Pool(10)
+p.map(scraper, range(0,len(data),200))
 
